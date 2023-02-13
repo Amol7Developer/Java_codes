4f4431f9ded65790034c4576cbcf82c208972596
Amol7Developer <amol0783com@gmail.com>
2023-02-13 20:23:38 +0530

    String print UpperCase and LowerCase form with methods

---
+++ UpperLowerCase.java
@@ -0,0 +1,12 @@
+public class UpperLowerCase {
+    public static void main (String [] args){
+    
+    //string variable declare 
+    String txt = "Hello World";
+ 
+    //String print with upper case and lower case using toUpperCase() and toLowerCase() methods
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+
+    }
+}
