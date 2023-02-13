9d9e8694bfa78daf9c15205dbb71d184f07740f4
Amol7Developer <amol0783com@gmail.com>
2023-02-13 20:03:04 +0530

    String length count with help of length() method

---
+++ LengthCountWithFunction.java
@@ -0,0 +1,12 @@
+public class LengthCountWithFunction {
+     public static void main(String[] args) {
+    
+    //declare string variable
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    //print string length 
+    System.out.println("The length of a string can be found with the length() method:");
+    System.out.println("The length of the txt:'"+txt+ "' string is: " + txt.length());
+  
+  }
+}
