c786c0d75ccb0fa0d405ee6860672f07e675b166
Amol7Developer <amol0783com@gmail.com>
2023-02-16 16:53:30 +0530

    Java String Method charAt() used

---
+++ StringBuiltingMethods/StringMethod1.java
@@ -0,0 +1,11 @@
+public class StringMethod1 {
+    public static void main(String[] args) {
+    String myStr = "Hello";
+
+    //The charAt() method returns the character at the specified index in a string.
+    //The index of the first character is 0, the second character is 1, and so on.
+    char result = myStr.charAt(0);
+    System.out.println("Java String charAt() Method used");
+    System.out.println(result);
+  }
+}
