ad8934102eb22669ccafe881eb4b5e97c9638eeb
Amol7Developer <amol0783com@gmail.com>
2023-02-17 11:17:50 +0530

    java String concat() method used

---
+++ StringBuiltingMethods/StringMethod7.java
@@ -0,0 +1,12 @@
+public class StringMethod7 {
+    public static void main(String [] args){
+        String firstName = "John ";
+        String lastName = "Doe";
+        
+//The concat() method appends (concatenate) a string to the end of another string.
+
+        System.out.println("Java String concat() Method used");
+
+        System.out.println(firstName.concat(lastName));
+    }
+}
