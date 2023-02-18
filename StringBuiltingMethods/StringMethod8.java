c60c545f80a52c0812c597a481751e8c70de3ef8
Amol7Developer <amol0783com@gmail.com>
2023-02-18 11:00:21 +0530

    update

--- StringBuiltingMethods/StringMethod8.java
+++ StringBuiltingMethods/StringMethod8.java
@@ -6,6 +6,8 @@ public class StringMethod8 {
 //The contains() method checks whether a string contains a sequence of characters.
 //Returns true if the characters exist and false if not.
 
+ System.out.println("Java String contains() Method used");
+  
     System.out.println(myStr.contains("Hel")); //true.
     System.out.println(myStr.contains("e")); //true.
     System.out.println(myStr.contains("Hi")); //false.
