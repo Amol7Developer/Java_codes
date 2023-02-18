d0c434f2106475106cb59ded0c8fb92a163abda1
Amol7Developer <amol0783com@gmail.com>
2023-02-18 11:30:37 +0530

    java string endWth() method used.

---
+++ StringBuiltingMethods/StringMethod11.java
@@ -0,0 +1,17 @@
+public class StringMethod11 {
+     public static void main(String[] args) {
+    String myStr = "Hello";
+
+
+//The endsWith() method checks whether a string ends with the specified character(
+//Tip: Use the startsWith() method to check whether a string starts with the specified character(s).
+ 
+
+     System.out.println("Java String endWith() Method used");
+
+    myStr2 = myStr2.copyValueOf(myStr1, 0, 5);
+    System.out.println(myStr.endsWith("Hel")); //false
+    System.out.println(myStr.endsWith("llo")); //ture
+    System.out.println(myStr.endsWith("o")); //ture
+  }
+}
