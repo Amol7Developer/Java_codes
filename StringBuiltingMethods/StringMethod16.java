07e92863d7a0cf90642636b7f3e60d31f5dc416a
Amol7Developer <amol0783com@gmail.com>
2023-02-19 21:02:04 +0530

    java String isEmpty() method used

---
+++ StringBuiltingMethods/StringMethod16.java
@@ -0,0 +1,14 @@
+public class StringMethod16 {
+    public static void main(String[] args) {
+    String myStr1 = "Hello";
+    String myStr2 = "";
+
+//The isEmpty() method checks whether a string is empty or not.
+//This method returns true if the string is empty (length() is 0), and false if not.
+
+    System.out.println("Java String isEmpty() Method used");
+
+    System.out.println(myStr1.isEmpty()); //false
+    System.out.println(myStr2.isEmpty()); //true
+  }
+}
