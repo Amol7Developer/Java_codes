f1cdcdb4b91fd56a47b79b55974c48b3a2e50502
Amol7Developer <amol0783com@gmail.com>
2023-02-20 16:27:48 +0530

    java string toUpperCase() and toLowerCase()methods used

---
+++ StringBuiltingMethods/StringMethod21.java
@@ -0,0 +1,15 @@
+public class StringMethod21 {
+    public static void main(String[] args) {
+    String txt = "Hello World";
+   
+   //The toLowerCase() method converts a string to lower case letters.
+  // Note: The toUpperCase() method converts a string to upper case letters.
+
+
+    System.out.println("Java String toUpperCase() and toLowerCase() Methods used");
+           
+   
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
