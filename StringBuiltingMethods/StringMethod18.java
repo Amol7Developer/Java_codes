d3f1c9090fa1f131e2845a8a767f1e8be4f43890
Amol7Developer <amol0783com@gmail.com>
2023-02-20 15:59:23 +0530

    java string length() method used

---
+++ StringBuiltingMethods/StringMethod18.java
@@ -0,0 +1,13 @@
+public class StringMethod18 {
+    public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   
+   //The length() method returns the length of a specified string.
+
+    //Note: The length of an empty string is 0.
+
+   System.out.println("Java String length() Method used");
+   
+    System.out.println(txt.length());
+  }
+}
