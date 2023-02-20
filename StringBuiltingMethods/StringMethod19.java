d75ca2b7a44b395168c10c268584beca0dccaff4
Amol7Developer <amol0783com@gmail.com>
2023-02-20 16:03:10 +0530

    java string replace() method used

---
+++ StringBuiltingMethods/StringMethod19.java
@@ -0,0 +1,12 @@
+public class StringMethod19 {
+    public static void main(String[] args) {
+    String myStr = "Hello";
+   
+   //The replace() method searches a string for a specified character, and returns a new string where the specified character(s) are replaced.
+
+   
+   System.out.println("Java String replace() Method used");
+   
+    System.out.println(myStr.replace('l', 'p'));
+  }
+}
