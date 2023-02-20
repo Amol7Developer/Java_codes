0d40be2a82b5866504b55da1e8d893e5c318e5e8
Amol7Developer <amol0783com@gmail.com>
2023-02-20 17:24:57 +0530

    java string trim() method used

---
+++ StringBuiltingMethods/StringMethod22.java
@@ -0,0 +1,13 @@
+public class StringMethod22 {
+     public static void main(String[] args) {
+    String myStr = "       Hello World!        ";
+    
+//The trim() method removes whitespace from both ends of a string.
+//Note: This method does not change the original string.
+
+     System.out.println("Java String trim() Method used");
+    
+    System.out.println(myStr);
+    System.out.println(myStr.trim());
+  }
+}
