55357e0ac4605bee57493c80f59144e4ab191eb6
Amol7Developer <amol0783com@gmail.com>
2023-02-21 22:16:52 +0530

    java Math abs() method used

---
+++ MathMethod1.java
@@ -0,0 +1,12 @@
+public class MathMethod1 {
+     public static void main(String[] args) {
+        
+        //The abs() method returns the absolute (positive) value of a number.
+        
+        System.out.println("Java Math abs() Method used");
+
+        System.out.println(Math.abs(-4.7));  
+        System.out.println(Math.abs(4.7));  
+        System.out.println(Math.abs(3));  
+  }
+}
